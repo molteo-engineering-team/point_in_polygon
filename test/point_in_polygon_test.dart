257f88f4064417dbc1437ee7b6b2f3fa7d13f2ea
aa-cee <a.abraham@cee-uk.com>
2024-02-02 15:46:29 +0000

    Update point_in_polygon_test.dart

--- test/point_in_polygon_test.dart
+++ test/point_in_polygon_test.dart
@@ -2,20 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:point_in_polygon/point_in_polygon.dart';
 
 void main() {
-  final List<Point> points = <Point>[
+  final Poly polygon = Poly(vertices: [
     Point(y: 42.412328554181684, x: -71.61572554715048),
     Point(y: 42.382918132678284, x: -71.63254836209188),
     Point(y: 42.36617846481582, x: -71.5951261819161),
     Point(y: 42.39306121437432, x: -71.5680036843575),
-  ];
+  ]);
 
   test('Point inside the polygon', () {
     final Point point = Point(x: -71.60473921902548, y: 42.38951132221119);
-    expect(Poly.isPointInPolygon(point, points), true);
+    expect(polygon.isPointInPolygon(point), true);
   });
 
   test('Point outside the polygon', () {
     final Point point = Point(x: -71.76850417263876, y: 42.38925775080263);
-    expect(Poly.isPointInPolygon(point, points), false);
+    expect(polygon.isPointInPolygon(point), false);
   });
 }
